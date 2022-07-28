142fb8d659f96ef34bb3ff6db21920f1043b7390
pan93412 <pan93412@gmail.com>
2022-07-28 12:14:08 +0000

    style: prettier

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
-import dotenv from "dotenv-defaults";
+import dotenv from 'dotenv-defaults';
 dotenv.config();
 
-import express from 'express';  
+import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
 const port = process.env.PORT || 4000;
 
 // GET request
 app.get('/getCourse/:name', async (req, res) => {
-    const name = req.params.name
-    console.log(req.url);
-    console.log(name);
-    
-    const regex = new RegExp(name, "i");
-    const existing = await Course.find({ name: {$regex: regex}});
-    res.status(200).send({ msg: 'OK', detail: existing })
+	const name = req.params.name;
+	console.log(req.url);
+	console.log(name);
+
+	const regex = new RegExp(name, 'i');
+	const existing = await Course.find({name: {$regex: regex}});
+	res.status(200).send({msg: 'OK', detail: existing});
 });
 
-
-
 // Run test
-app.listen(port, () =>
-  console.log(`API started on port ${port}!`),
-);
+app.listen(port, () => console.log(`API started on port ${port}!`));
 
 // Import mongoose
 import mongoose from 'mongoose';
 
 // Import model and schema
-import Course from './models/Course.js'
+import Course from './models/Course.js';
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((res) => console.log("MongoDB connection has been created."));
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((res) => console.log('MongoDB connection has been created.'));
 
-  const db = mongoose.connection;
-  db.on("error", (err) => console.log(err));
-  db.once("open", async () => {     
-  });
-
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err));
+db.once('open', async () => {});
