880b6d912f68b40f6be6b4422ce6da3bd5a20577
pan93412 <pan93412@gmail.com>
2022-07-28 13:01:00 +0000

    refactor!: extend and clean up the API

--- server.js
+++ server.js
@@ -1,30 +1,61 @@
+import process from 'node:process';
 import dotenv from 'dotenv-defaults';
-dotenv.config();
-
 import express from 'express';
-import bodyParser from 'body-parser';
-const app = express();
-const port = process.env.PORT || 4000;
-
-// GET request
-app.get('/getCourse/:name', async (req, res) => {
-	const name = req.params.name;
-	console.log(req.url);
-	console.log(name);
-
-	const regex = new RegExp(name, 'i');
-	const existing = await Course.find({name: {$regex: regex}});
-	res.status(200).send({msg: 'OK', detail: existing});
-});
-
-// Run test
-app.listen(port, () => console.log(`API started on port ${port}!`));
 
 // Import mongoose
 import mongoose from 'mongoose';
 
 // Import model and schema
-import Course from './models/Course.js';
+import Course from './models/course.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || '4000';
+
+app.get('/course/:id', async (request, response) => {
+	const {id} = request.params;
+
+	const document = await Course.findById(id);
+
+	if (document) {
+		// 如果有內容，則回傳 200。
+		response.status(200);
+	} else {
+		// 如果沒有內容，則回傳 404。
+		response.status(404);
+	}
+
+	// 無論有沒有內容都回傳 data。
+	response.send({data: document?.toJSON()});
+});
+
+app.get('/course/search', async (request, response) => {
+	const {keyword} = request.query;
+
+	if (!keyword) {
+		response.status(400).send({error: '缺少必要的參數 “keyword”。'});
+	}
+
+	const keywordMatcher = new RegExp(keyword, 'i');
+
+	const matchedResult = await Course.find({
+		name: {
+			$regex: keywordMatcher,
+		},
+	});
+
+	if (matchedResult?.length > 0) {
+		response.status(200);
+	} else {
+		response.status(400);
+	}
+
+	response.send({data: matchedResult.toJSON()});
+});
+
+// Run test
+app.listen(port, () => console.log(`API started on port ${port}!`));
 
 // Connect to MongoDB
 mongoose
@@ -32,8 +63,8 @@ mongoose
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
-	.then((res) => console.log('MongoDB connection has been created.'));
+	.then(() => console.log('MongoDB connection has been created.'));
 
 const db = mongoose.connection;
-db.on('error', (err) => console.log(err));
+db.on('error', (error) => console.log(error));
 db.once('open', async () => {});
